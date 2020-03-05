5b67972c1343d302b58f091c267bfee0665011fa
Luis Zayas <zayaslui@gmail.com>
2020-03-05 18:13:36 -0300

    update

---
+++ precache-manifest.7e0fefad9912c49dd5068bdfa98d8b04.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92bdcf29294236f1378828b2ffc10541",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d295824673b777351120",
+    "url": "/static/css/main.1e11935a.chunk.css"
+  },
+  {
+    "revision": "65702855d034e3772a72",
+    "url": "/static/js/2.44a65775.chunk.js"
+  },
+  {
+    "revision": "f1889b2eae3a2ddaf39d3210d1dfd806",
+    "url": "/static/js/2.44a65775.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d295824673b777351120",
+    "url": "/static/js/main.b8e9f6d7.chunk.js"
+  },
+  {
+    "revision": "6286d7163df321efe175e9f4f047310a",
+    "url": "/static/js/main.b8e9f6d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0c937fae627a5e8dd40",
+    "url": "/static/js/runtime-main.6960b511.js"
+  },
+  {
+    "revision": "dfbf459c7e39ae2e0077b8fa4dc04411",
+    "url": "/static/media/antoine-barres.dfbf459c.jpg"
+  },
+  {
+    "revision": "bfc688ca490f0079051aea5b8ec75cbe",
+    "url": "/static/media/ayo-ogunseinde-2.bfc688ca.jpg"
+  },
+  {
+    "revision": "bb4b9ac9fd07e48f86c719495b65341c",
+    "url": "/static/media/clem-onojeghuo-2.bb4b9ac9.jpg"
+  },
+  {
+    "revision": "ae11d928aae96245020bb59a1910f758",
+    "url": "/static/media/clem-onojeghuo-3.ae11d928.jpg"
+  },
+  {
+    "revision": "61c4e0007254eb55e4691291881a4bfb",
+    "url": "/static/media/clouds.61c4e000.png"
+  },
+  {
+    "revision": "facbbdb787d77f975904ba9fa99b15c6",
+    "url": "/static/media/daniel-olahh.facbbdb7.jpg"
+  },
+  {
+    "revision": "2051e0e847124c536df9c01a3f672032",
+    "url": "/static/media/erik-lucatero-2.2051e0e8.jpg"
+  },
+  {
+    "revision": "f26c34bb88eb83d5aae99c7dbf3ddd48",
+    "url": "/static/media/fabio-mangione.f26c34bb.jpg"
+  },
+  {
+    "revision": "7812184c3710d9623c684956fe048ddd",
+    "url": "/static/media/fog-low.7812184c.png"
+  },
+  {
+    "revision": "76ff47ae5c64ccda5af18326bb272058",
+    "url": "/static/media/joe-gardner-2.76ff47ae.jpg"
+  },
+  {
+    "revision": "be9fa468a029a195601370492c15519d",
+    "url": "/static/media/login-image.be9fa468.jpg"
+  },
+  {
+    "revision": "48ac036da7c7923266266f63746fc5f1",
+    "url": "/static/media/nucleo-icons.48ac036d.ttf"
+  },
+  {
+    "revision": "5c78b4cf03771fd2287325c44f0d3cd9",
+    "url": "/static/media/nucleo-icons.5c78b4cf.woff2"
+  },
+  {
+    "revision": "6836b93bf1af0b571928d98458a68445",
+    "url": "/static/media/nucleo-icons.6836b93b.woff"
+  },
+  {
+    "revision": "f32ed5ad3d76067b8e43be1a0bd1ba91",
+    "url": "/static/media/nucleo-icons.f32ed5ad.eot"
+  },
+  {
+    "revision": "2bf06320e2f5a18fa645e58800a2204b",
+    "url": "/static/media/principal.2bf06320.png"
+  }
+]);
